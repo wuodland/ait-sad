954e4457af42564baba4872808fd190eb51ffefe
abhinavsingha <abhinavsingha@outlook.com>
2019-02-05 22:43:03 +0700

    routes update

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_02_05_153653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "infos", force: :cascade do |t|
+    t.string "First_name"
+    t.string "Last_name"
+    t.string "email"
+    t.string "Address"
+    t.string "gender"
+    t.integer "Age"
+    t.bigint "PhoneNumber"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
